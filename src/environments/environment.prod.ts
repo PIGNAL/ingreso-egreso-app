245957cf9f7b607e4a6ab2d76c546b136be9f696
PIGNAL <joni_ballatore@hotmail.com>
2020-10-03 12:50:40 -0300

    Seccion 7 - Auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCV_d_QY8GPhzp7Dz9yfh0XWTWhD-G64R4',
+    authDomain: 'ingreso-egreso-app-6d925.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-6d925.firebaseio.com',
+    projectId: 'ingreso-egreso-app-6d925',
+    storageBucket: 'ingreso-egreso-app-6d925.appspot.com',
+    messagingSenderId: '161894620281',
+    appId: '1:161894620281:web:0b324858f573670f9288a8'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCV_d_QY8GPhzp7Dz9yfh0XWTWhD-G64R4',
+    authDomain: 'ingreso-egreso-app-6d925.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-6d925.firebaseio.com',
+    projectId: 'ingreso-egreso-app-6d925',
+    storageBucket: 'ingreso-egreso-app-6d925.appspot.com',
+    messagingSenderId: '161894620281',
+    appId: '1:161894620281:web:0b324858f573670f9288a8'
+  }
 };
 
 /*
